0ff5d96e9e32f712a0e97228e10ea646cc6fafc1
kamsoft <wizytadomowakamsoft@gmail.com>
2019-11-10 10:16:26 +0100

    my first repository

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace MyBot1
+{
+    using System;
+    using MargieBot;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("...");
+            Bot mybot = new Bot();
+            mybot.Connect("xoxb-816926727939-816929558451-KjCljodsuz1SzHMwMIlGVE9g");
+            Console.WriteLine("...");
+            Console.Read();
+        }
+    }
+}
